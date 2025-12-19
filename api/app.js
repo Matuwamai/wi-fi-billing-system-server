ba77e52dc8e3695b6276572fe5a713377dedbe66
Matuwamai <wamaimatu@gmail.com>
2025-12-19 15:14:33 +0300

    Create voucher router

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ import subscriptionRoutes from "./routes/subscription.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import mpeasaRoutes from "./routes/mpesa.js";
 import routerSessionRoutes from "./routes/routerSession.js";
+import voucherRouetes from "./routes/voucher.js";
 import logger from "./utils/logger.js";
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/subscriptions", subscriptionRoutes);
 app.use("/api/session", routerSessionRoutes);
 app.use("/api/mpesa", mpeasaRoutes);
+app.use("/api/vouchers", voucherRouetes);
 
 // Error handler
 app.use(errorHandler);
