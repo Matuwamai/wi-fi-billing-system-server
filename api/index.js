454e5d87bb45f5cc249dba29dc9d342d90d318ba
Matuwamai <wamaimatu@gmail.com>
2026-01-05 10:17:59 +0300

    Update port to 5005

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
 
 // Start background job
