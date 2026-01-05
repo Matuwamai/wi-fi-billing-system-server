718e91c9ac0e7c1b51ce144076f2136bfd7c52ff
Matuwamai <wamaimatu@gmail.com>
2026-01-05 10:27:40 +0300

    Enhance index.js file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
 import app from "./app.js";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5005;
+const prisma = new PrismaClient();
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await prisma.$connect(); // wait for DB
+    console.log("✅ Prisma connected");
 
-// Start background job
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server", err);
+    process.exit(1);
+  }
+}
+
+startServer();
